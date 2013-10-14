3afc8a66f4e161c21c6286ace0daa25e897e914c
Drew Blaisdell <drew.blaisdell@gmail.com>
2013-10-13 17:47:41 -0700

    signup page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,36 +3,36 @@ require 'spec_helper'
 describe "StaticPages" do
 	describe "Home page" do
 		it "should have the right title" do
-			visit '/static_pages/home'
-			expect(page).to have_title('Sample App | Home')
+			visit home_path
+			expect(page).to have_title('Sample App!')
 		end
 
 		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
+			visit home_path
 			expect(page).to have_content('Sample App')
 		end
 	end
 
 	describe "Help page" do
 		it "should have the right title" do
-			visit '/static_pages/help'
-			expect(page).to have_title('Sample App | Help')
+			visit help_path
+			expect(page).to have_title('Sample App! | Help')
 		end
 		
 		it "should have the content 'Help'" do
-			visit '/static_pages/help'
+			visit help_path
 			expect(page).to have_content('Help')
 		end
 	end
 
 	describe "About page" do
 		it "should have the right title" do
-			visit '/static_pages/about'
-			expect(page).to have_title('Sample App | About Us')
+			visit about_path
+			expect(page).to have_title('Sample App! | About Us')
 		end
 		
 		it "should have the content 'About Us'" do
-			visit '/static_pages/about'
+			visit about_path
 			expect(page).to have_content('About Us')
 		end
 	end
